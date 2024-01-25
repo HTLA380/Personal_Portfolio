b72f05dc3a24be1a2b9c491293408b3d328d0d2c
Htet Aung Lin <113810462+HTLA380@users.noreply.github.com>
2024-01-25 10:09:12 +0000

    add ReactBoard project

--- src/Pages/Projects/Project-data.js
+++ src/Pages/Projects/Project-data.js
@@ -1,47 +1,29 @@
-import Project1 from "../../assets/projects/project-1.jpeg";
-import Project2 from "../../assets/projects/project-2.jpeg";
+import Project6 from "../../assets/projects/project-6.jpeg";
+import Project7 from "../../assets/projects/project-7.jpeg";
+import Project1 from "../../assets/projects/project-1.png";
+import Project2 from "../../assets/projects/project-2.png";
 import Project3 from "../../assets/projects/project-3.jpeg";
 import Project4 from "../../assets/projects/project-4.jpeg";
 import Project5 from "../../assets/projects/project-5.jpeg";
-import Project6 from "../../assets/projects/project-6.png";
 
 export default [
   {
-    title: "EasyBank Landing Page",
+    title: "React Board",
     img: Project1,
-    techs: ["HTML", "Sass & Css", "Javascript"],
-    webSiteLink: "https://htl-easybank-landing.netlify.app/",
-    source: "https://github.com/HTLA380/EasyBank-Landing-Page.git",
+    techs: ["React", "Typescript", "Material UI", "Chart.js"],
+    webSiteLink: "https://htl-react-board.netlify.app/",
+    source: "https://github.com/HTLA380/React-Dashboard",
   },
-
   {
-    title: "Sunnyside agency landing page",
+    title: "Cobalt Clone",
     img: Project2,
-    techs: ["HTML", "Sass & Css", "Javascript"],
-    webSiteLink: "https://htla-tech.netlify.app/",
-    order: false,
-    source: "https://github.com/HTLA380/SunnySide-Landing-Page.git",
+    techs: ["React Js", "Tailwind Css", "Framer Motion", "TypeWriter Effect"],
+    webSiteLink: "https://cobalt-clone.netlify.app/",
+    source: "https://github.com/HTLA380/Cobalt-Clone",
   },
-
-  {
-    title: "E-commerce product page",
-    img: Project3,
-    techs: ["HTML", "Sass & Css", "Javascript"],
-    webSiteLink: "https://e-commerce-page-htl.netlify.app/",
-    source: "https://github.com/HTLA380/E-commerce-product-page.git",
-  },
-
-  {
-    title: "REST Countries API with color theme switcher",
-    img: Project4,
-    techs: ["HTML", "Css", "BootStrap", "Javascript", "REST API"],
-    webSiteLink: "https://rest-county-api-htl.netlify.app/",
-    source: "https://github.com/HTLA380/REST-Countries-API.git",
-  },
-
   {
     title: "Recipe App Using React JS",
-    img: Project5,
+    img: Project3,
     techs: ["HTML", "Css", "React", "styled-components"],
     webSiteLink: "https://htla-recipe-app.netlify.app/",
     content:
@@ -49,10 +31,32 @@ export default [
     source: "https://github.com/HTLA380/Recipe-App.git",
   },
   {
-    title: "Cobalt Clone",
+    title: "REST Countries API with color theme switcher",
+    img: Project5,
+    techs: ["HTML", "Css", "BootStrap", "Javascript", "REST API"],
+    webSiteLink: "https://rest-county-api-htl.netlify.app/",
+    source: "https://github.com/HTLA380/REST-Countries-API.git",
+  },
+  {
+    title: "E-commerce product page",
+    img: Project4,
+    techs: ["HTML", "Sass & Css", "Javascript"],
+    webSiteLink: "https://e-commerce-page-htl.netlify.app/",
+    source: "https://github.com/HTLA380/E-commerce-product-page.git",
+  },
+  {
+    title: "EasyBank Landing Page",
     img: Project6,
-    techs: ["React Js", "Tailwind Css", "Framer Motion", "TypeWriter Effect"],
-    webSiteLink: "https://cobalt-clone.netlify.app/",
-    source: "https://github.com/HTLA380/Desktop-Cobalt-Clone.git",
+    techs: ["HTML", "Sass & Css", "Javascript"],
+    webSiteLink: "https://htl-easybank-landing.netlify.app/",
+    source: "https://github.com/HTLA380/EasyBank-Landing-Page.git",
+  },
+  {
+    title: "Sunnyside agency landing page",
+    img: Project7,
+    techs: ["HTML", "Sass & Css", "Javascript"],
+    webSiteLink: "https://htla-tech.netlify.app/",
+    order: false,
+    source: "https://github.com/HTLA380/SunnySide-Landing-Page.git",
   },
 ];
