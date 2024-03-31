b25e383281c8c69142ede8a4db00a72ca3386e75
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-31 13:10:10 +0630

    feat: add page loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./css/loading.css";
+
+const Loader = () => {
+  return (
+    <div className="w-full h-screen bg-gray-300">
+      <div className="loading-window ">
+        <div className="car">
+          <div className="strike"></div>
+          <div className="strike strike2"></div>
+          <div className="strike strike3"></div>
+          <div className="strike strike4"></div>
+          <div className="strike strike5"></div>
+          <div className="car-detail spoiler"></div>
+          <div className="car-detail back"></div>
+          <div className="car-detail center"></div>
+          <div className="car-detail center1"></div>
+          <div className="car-detail front"></div>
+          <div className="car-detail wheel"></div>
+          <div className="car-detail wheel wheel2"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
